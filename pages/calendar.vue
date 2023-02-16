9296e161ec7dd15f85a95a8e757e5e2ab950f208
Ennio Marke <mystical.ennio@gmail.com>
2023-02-16 10:53:31 -0600

    init

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,11 +1,30 @@
 <script setup>
-import { NButton } from 'naive-ui'
-</script>
-<script>
-export default {}
+import { NButton, NCalendar, useMessage } from 'naive-ui'
+
+let events = [
+    {
+        data: "2021-10-01",
+    }
+]
+
+defineNuxtComponent({
+    setup() {
+        const message = useMessage();
+        return {
+            value: ref(new Date()),
+            handleUpdateValue(_, { year, month, date }) {
+                console.log(`${year}-${month}-${date}`)
+                message.success(`${year}-${month}-${date}`);
+            },
+        }
+    }
+})
 </script>
 <template>
-    <div>
-        <n-button>naive-ui</n-button>
+    <div class="p-5">
+        <h1>Familien Kalender</h1>
+        <n-calendar v-model:value="value" #="{ year, month, date }" @update:value="handleUpdateValue()">
+            {{ date }}.{{ month }}.{{ year }}
+        </n-calendar>
     </div>
 </template>
