227cd654b6f965de3bda4d68a66ea2538ff631f9
Ennio Marke <mystical.ennio@gmail.com>
2023-02-17 13:31:40 -0600

    FullCalendar

--- pages/calendar.vue
+++ pages/calendar.vue
@@ -1,30 +1,49 @@
 <script setup>
-import { NButton, NCalendar, useMessage } from 'naive-ui'
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+const nuxtApp = useNuxtApp();
 
-let events = [
+const calendarOptions = ref(
     {
-        data: "2021-10-01",
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth',
+        events: [
+            { title: "event 1", date: "2023-02-20" },
+            { title: "event 2", date: "2023-02-23" },
+        ],
     }
-]
+)
 
-defineNuxtComponent({
-    setup() {
-        const message = useMessage();
-        return {
-            value: ref(new Date()),
-            handleUpdateValue(_, { year, month, date }) {
-                console.log(`${year}-${month}-${date}`)
-                message.success(`${year}-${month}-${date}`);
-            },
-        }
+
+nuxtApp.vueApp.component("FullCalendar", FullCalendar)
+
+
+let events = ref([
+    {
+        date: {
+            year: 2023,
+            month: 2,
+            date: 20
+        },
+        description: "Go to the gym"
+    },
+    {
+        date: {
+            year: 2023,
+            month: 2,
+            date: 22
+        },
+        description: "Go cook"
     }
+])
+
+onMounted(() => {
+    document.querySelector(".fc-header-toolbar").children[1].innerHTML = "<h1>Familien Kalendar</h1>"
 })
 </script>
 <template>
     <div class="p-5">
-        <h1>Familien Kalender</h1>
-        <n-calendar v-model:value="value" #="{ year, month, date }" @update:value="handleUpdateValue()">
-            {{ date }}.{{ month }}.{{ year }}
-        </n-calendar>
+        <FullCalendar :options='calendarOptions' />
     </div>
 </template>
